f2e37501d13a22e70bed5df12e9961c54937aed6
Островський Андрій <106969285+OstrovskyAndriy@users.noreply.github.com>
2022-10-12 10:28:43 +0300

    Add files via upload

---
+++ lab_5/lab_5-1.cpp
@@ -0,0 +1,291 @@
+﻿// lab_5-1.cpp : 
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+//class Publication {
+//	string name;
+//	float price;
+//
+//public:
+//
+//	Publication() {
+//		name = "";
+//		price = 0.0;
+//	}
+//
+//	Publication(string name, float price) {
+//		this->name = name;
+//		this->price = price;
+//	}
+//
+//	virtual void getData() {
+//		cout << "Price = " << price << "\tname: " << name << "\t";
+//	}
+//
+//	virtual void putData() {
+//		cout << "enter name: ";
+//		getline(cin, name);
+//
+//		cout << "Enter price: ";
+//		cin >> price;
+//	}
+//};
+//
+//class Book : public Publication {
+//	int nOfPages;
+//public:
+//
+//	Book() {
+//		nOfPages = 0;
+//	}
+//
+//	Book(string name, float price, int nOfPages) : Publication(name, price) {
+//		this->nOfPages = nOfPages;
+//	}
+//
+//	void getData() override {
+//		Publication::getData();
+//		cout << "Number of pages = " << nOfPages << endl;
+//	}
+//
+//	void putData() override {
+//		Publication::putData();
+//
+//		cout << "Number of pages = ";
+//		cin >> nOfPages;
+//	}
+//};
+//
+//
+//
+//class Type : public Publication {
+//	float time;
+//public:
+//
+//	Type() {
+//		time = 0;
+//	}
+//
+//	Type(string name, float price, float time) : Publication(name, price) {
+//		this->time = time;
+//
+//	}
+//
+//	void getData() override {
+//		Publication::getData();
+//		cout << "time = " << time << endl;
+//	}
+//
+//	void putData() override {
+//		Publication::putData();
+//		cout << "time = ";
+//		cin >> time;
+//	}
+//};
+//
+//
+//int menu() {
+//	int item;
+//	cout << "1 - add new book" << endl;
+//	cout << "2 - add new audio book" << endl;
+//	cin >> item;
+//	return item;
+//}
+//
+//
+//int main()
+//{
+//	Publication* publication[4];
+//
+//	int count = 0;
+//
+//	int menuChoice;
+//
+//	while ((menuChoice = menu())) {
+//
+//		cin.ignore();
+//
+//		switch (menuChoice)
+//		{
+//		case 1:
+//			publication[count] = new Book;
+//			publication[count]->putData();
+//			system("cls");
+//			break;
+//
+//		case 2:
+//			publication[count] = new Type;
+//			publication[count]->putData();
+//			system("cls");
+//		}
+//
+//		if (count == 3) {
+//			break;
+//		}
+//
+//		count++;
+//	}
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		publication[i]->getData();
+//		delete publication[i];
+//	}
+//}
+
+class Publication {
+	string name;
+	float price;
+
+public:
+
+	Publication() {
+		name = "";
+		price = 0.0;
+	}
+
+	Publication(string name, float price) {
+		this->name = name;
+		this->price = price;
+	}
+
+	virtual void getData() {
+		cout << "Price = " << price << "\tname: " << name << "\t";
+	}
+
+	virtual void putData() {
+		cout << "enter name: ";
+		getline(cin, name);
+
+		cout << "Enter price: ";
+		cin >> price;
+	}
+
+	bool virtual isOveersize() {
+		return 0;
+	}
+};
+
+class Book : public Publication {
+	int nOfPages;
+public:
+
+	Book() {
+		nOfPages = 0;
+	}
+
+	Book(string name, float price, int nOfPages) : Publication(name, price) {
+		this->nOfPages = nOfPages;
+	}
+
+	void getData() override {
+		Publication::getData();
+		cout << "Number of pages = " << nOfPages << endl;
+	}
+
+	void putData() override {
+		Publication::putData();
+
+		cout << "Number of pages = ";
+		cin >> nOfPages;
+	}
+
+	bool isOveersize() override {
+		return nOfPages > 800;
+	}
+};
+
+
+
+class Type : public Publication {
+	float time;
+public:
+
+	Type() {
+		time = 0;
+	}
+
+	Type(string name, float price, float time) : Publication(name, price) {
+		this->time = time;
+
+	}
+
+	void getData() override {
+		Publication::getData();
+		cout << "time = " << time << endl;
+	}
+
+	void putData() override {
+		Publication::putData();
+		cout << "time = ";
+		cin >> time;
+	}
+
+	bool isOveersize() override {
+		return time > 90;
+	}
+};
+
+
+int menu() {
+	int item;
+	cout << "1 - add new book" << endl;
+	cout << "2 - add new audio book" << endl;
+	cin >> item;
+	return item;
+}
+
+
+int main()
+{
+	Publication* publication[4];
+
+	int count = 0;
+
+	int menuChoice;
+
+	while ((menuChoice = menu())) {
+
+		cin.ignore();
+
+		switch (menuChoice)
+		{
+		case 1:
+			publication[count] = new Book;
+			publication[count]->putData();
+			system("cls");
+			break;
+
+		case 2:
+			publication[count] = new Type;
+			publication[count]->putData();
+			system("cls");
+		}
+
+		if (count == 3) {
+			break;
+		}
+
+		count++;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		cout << i << ")  ";
+		if (!publication[i]->isOveersize()) {
+			
+			publication[i]->getData();
+		}
+
+		else {
+			cout << "publication[" << i << "]" << " is oversize" << endl;
+		}
+
+
+		delete publication[i];
+	}
+}
+
